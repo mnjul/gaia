7b7cecfcbaf440e925edb0c110e6827760e97ec0
John Lu [:mnjul] <jlu@mozilla.com>
2014-07-24 19:16:00 +0800

    variable rename

--- apps/system/js/ime_switcher.js
+++ apps/system/js/ime_switcher.js
@@ -8,60 +8,60 @@
    * and may be interacted upon for showing IME selection menu. 
    */
   var IMESwitcher = function() {
-    this._notifIMEContainer = null;
-    this._fakenoti = null;
-    this._fakenotiMessage = null;
-    this._fakenotiTip = null;
+    this._utilityTrayContainer = null;
+    this._notificationContainer = null;
+    this._notificationTitle = null;
+    this._notificationTip = null;
     this._showAllCallback = undefined;
   };
 
   IMESwitcher.prototype.start = function is_start(showAllCallback) {
-    this._notifIMEContainer =
+    this._utilityTrayContainer =
       document.getElementById('keyboard-show-ime-list');
 
-    this._fakenoti =
-      this._notifIMEContainer.querySelector('.fake-notification');
-    this._fakenotiMessage = this._fakenoti.querySelector('.message');
-    this._fakenotiTip = this._fakenoti.querySelector('.tip');
+    this._notificationContainer =
+      this._utilityTrayContainer.querySelector('.fake-notification');
+    this._notificationTitle = this._notificationContainer.querySelector('.message');
+    this._notificationTip = this._notificationContainer.querySelector('.tip');
 
-    this._fakenoti.addEventListener('mousedown', this);
+    this._notificationContainer.addEventListener('mousedown', this);
 
     this._showAllCallback = showAllCallback;
   };
 
   IMESwitcher.prototype.stop = function is_stop() {
-    this._fakenoti.removeEventListener('mousedown', this);
-    this._notifIMEContainer = null;
-    this._fakenoti = null;
-    this._fakenotiMessage = null;
-    this._fakenotiTip = null;
+    this._notificationContainer.removeEventListener('mousedown', this);
+    this._utilityTrayContainer = null;
+    this._notificationContainer = null;
+    this._notificationTitle = null;
+    this._notificationTip = null;
     this._showAllCallback = undefined;
   };
 
   IMESwitcher.prototype.show = function is_show(appName_, imeName) {
     window.dispatchEvent(new CustomEvent('keyboardimeswitchershow'));
 
-    navigator.mozL10n.localize(this._fakenotiMessage, 'ime-switching-title', {
+    navigator.mozL10n.localize(this._notificationTitle, 'ime-switching-title', {
       appName: appName_,
       name: imeName
     });
-    navigator.mozL10n.localize(this._fakenotiTip, 'ime-switching-tip');
+    navigator.mozL10n.localize(this._notificationTip, 'ime-switching-tip');
 
     // Instead of create DOM element dynamically, we can just turn the message
     // on/off and add message as we need. This save the time to create and
     // append element.
-    this._fakenoti.classList.add('activated');
+    this._notificationContainer.classList.add('activated');
   };
 
   IMESwitcher.prototype.hide = function is_hide() {
-    this._fakenoti.classList.remove('activated');
+    this._notificationContainer.classList.remove('activated');
     window.dispatchEvent(new CustomEvent('keyboardimeswitcherhide'));
   };
 
   IMESwitcher.prototype.handleEvent = function is_handleEvent(evt) {
     switch(evt.type){
       case 'mousedown':
-        if(evt.currentTarget === this._fakenoti){
+        if(evt.currentTarget === this._notificationContainer){
           evt.preventDefault();
           this._showAllCallback();
         }
