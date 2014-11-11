94f631d89d5caa394ded2fb409df3434ae997084
John Lu [:mnjul] <jlu@mozilla.com>
2014-11-11 16:58:00 +0800

    Fix MockKM

--- apps/system/test/unit/mock_keyboard_manager.js
+++ apps/system/test/unit/mock_keyboard_manager.js
@@ -1,16 +1,9 @@
 MockKeyboardManager = {
   mHeight: 0,
   init: function() {},
-  removeKeyboard: function() {},
-  hideKeyboard: function() {},
-  getHeight: function() {
-    return this.mHeight;
-  },
   mTeardown: function() {
     this.mHeight = 0;
   },
   _onKeyboardReady: function() {},
-  inputTypeTable: {},
-  isOutOfProcessEnabled: false,
-  totalMemory: 0
+  inputTypeTable: {}
 };
